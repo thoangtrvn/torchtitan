c495edd9b0fc4c356fbaba937a76551fe13e6377
TUAN HOANG TRONG <tmhoangt@us.ibm.com>
2024-06-11 19:01:28 +0000

    feat: add train script FP8

---
+++ run_llama2_train_fp8.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+# LOG_RANK=0,1 NGPU=2 ./run_llama2_train_fp8.sh 2>&1 | tee output_llama2.txt
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+set -ex
+
+# libUV is a scalable backend for TCPStore which is used in processGroup
+# rendezvous. This is the recommended backend for distributed training.
+export USE_LIBUV=1
+TRAINER_DIR=${1:-/home/$USER/local/torchtitan}
+
+# use envs as local overrides for convenience
+# e.g.
+# LOG_RANK=0,1 NGPU=4 ./run_llama_train.sh
+
+NGPU=${NGPU:-"1"}
+
+# by default log just rank 0 output,
+LOG_RANK=${LOG_RANK:-0}
+
+
+CONFIG_FILE=${CONFIG_FILE:-"./train_configs/llama2_1.4b.toml"}
+
+overrides=""
+if [ $# -ne 0 ]; then
+    overrides="$*"
+fi
+
+torchrun --nproc_per_node=${NGPU} --rdzv_backend c10d --rdzv_endpoint="localhost:0" \
+--local-ranks-filter ${LOG_RANK} --role rank --tee 3 \
+train.py --job.config_file ${CONFIG_FILE} $overrides
